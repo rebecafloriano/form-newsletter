d1456280a2261ab494b580b102345675da59f84f
nomeRebeca <rebecaoliver09@gmail.com>
2026-07-14 14:32:23 +0100

    fix: robust exclusion of playwright tests in vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig, configDefaults } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
@@ -7,7 +7,11 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/setupTests.ts',
-    exclude: ['**/node_modules/**', '**/dist/**', 'tests/**'],
+    
+    exclude: [
+      ...configDefaults.exclude,
+      '**/tests/**', 
+    ],
   },
   base: '/form-newsletter/',
 })
